20d676024511b0e6185693c38647c94832443d92
Ross Johnstone <rhjohnstone@gmail.com>
2017-04-10 13:15:03 +0100

    Try computing BFs for all drugs/channels

--- bayes_factors/dose_response/python/compute_all_bayes_factors.py
+++ bayes_factors/dose_response/python/compute_all_bayes_factors.py
@@ -41,12 +41,15 @@ def compute_log_pys(drug_channel):
 
     return None
 
-"""for drug_channel in it.product(drugs_to_run, channels_to_run):
+for drug_channel in it.product(drugs_to_run, channels_to_run):
     try:
+        print "\n"
+        print drug_channel
         compute_log_pys(drug_channel)
+        print "\n"
     except:
         print "no log_pys for", drug_channel
-        continue"""
+        continue
 
-drug_channel = ('Amiodarone', 'hERG')
-compute_log_pys(drug_channel)
+"""drug_channel = ('Amiodarone', 'hERG')
+compute_log_pys(drug_channel)"""
