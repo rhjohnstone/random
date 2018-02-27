7f17375a957ea42a4a40ac36a46906e02350e4ac
Ross Johnstone <rhjohnstone@gmail.com>
2018-02-27 20:19:56 +0900

    OO Python practice

---
+++ oopython/rpg.py
@@ -0,0 +1,33 @@
+class LivingThing:
+    """RPG-type character"""
+    def __init__(self, name, health, magicPoints, inventory):
+        self.name = name
+        self._health = health
+        self.magicPoints = magicPoints
+        self.inventory = inventory
+        self.hunger = 0  # all living things start with hunger level 0
+
+    def takeDamage(self, dmgAmount):
+        self._health -= dmgAmount
+        if self._health <= 0:
+            self._health = 0
+            print self.name + ' is dead!'
+
+class Dragon(LivingThing):
+    def __init__(self, name, health, magicPoints, inventory, wingspan):
+        LivingThing.__init__(self, name, health, magicPoints, inventory)
+        self._wingspan = wingspan
+    
+
+# Create the LivingThing object for the hero.
+hero = LivingThing('Elsa', 50, 80, {})
+monsters = []
+monsters.append(LivingThing('Goblin', 20, 0, {'gold': 12, 'dagger': 1}))
+monsters.append(LivingThing('Dragon', 300, 200, {'gold': 890, 'magic amulet': 1}))
+
+print 'The hero {} has {} health.'.format(hero.name, hero._health)
+
+
+drogon = Dragon('Drogon', 20, 0, {'gold': 12, 'dagger': 1}, 100)
+
+
