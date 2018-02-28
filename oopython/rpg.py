65ce511623d2c11c253f583a66ce985a12f93b2d
Ross Johnstone <rhjohnstone@gmail.com>
2018-02-28 16:42:38 +0900

    Python class tutorial

--- oopython/rpg.py
+++ oopython/rpg.py
@@ -1,4 +1,4 @@
-class LivingThing:
+class LivingThing(object):
     """RPG-type character"""
     def __init__(self, name, health, magicPoints, inventory):
         self.name = name
@@ -6,12 +6,16 @@ class LivingThing:
         self.magicPoints = magicPoints
         self.inventory = inventory
         self.hunger = 0  # all living things start with hunger level 0
+        self.__age = 50
 
     def takeDamage(self, dmgAmount):
         self._health -= dmgAmount
         if self._health <= 0:
             self._health = 0
             print self.name + ' is dead!'
+    
+    def getAge(self):
+        return self.__age
 
 class Dragon(LivingThing):
     def __init__(self, name, health, magicPoints, inventory, wingspan):
@@ -26,7 +30,7 @@ monsters.append(LivingThing('Goblin', 20, 0, {'gold': 12, 'dagger': 1}))
 monsters.append(LivingThing('Dragon', 300, 200, {'gold': 890, 'magic amulet': 1}))
 
 print 'The hero {} has {} health.'.format(hero.name, hero._health)
-
+print hero.getAge()
 
 drogon = Dragon('Drogon', 20, 0, {'gold': 12, 'dagger': 1}, 100)
 
